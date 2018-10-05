92990414dac90d5173ae7a51b8c2549e7636ac23
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-10-05 09:16:31 +0900

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		// 起動メッセージ
+		System.out.println("DBP ~ DataBaseProgram~");
+		System.out.println();
+
+		// データベースの新規作成 | データベースファイル読み込み
+		Database db;
+		System.out.println("データベース: 新規作成(new) | 開く(open) | 一覧表示(list)");
+		System.out.print("> ");
+		Scanner sc = new Scanner(System.in);
+		switch (sc.next()) {
+			case "new":
+				System.out.println("データベース名 >");
+				db = new Database();
+				db.setName(sc.next());
+				break;
+			case "open":
+				System.out.println("データベース名 >");
+				db = new Database();
+				db.
+				break;
+			case "list":
+				break;
+			default:
+				System.out.println("エラー");
+		}
+
+		// 中身dorの操作
+		s
+			casew "asddd" wwitch {
+		}
+	}
+
+	static void add() {
+		db.tbl["成績"].add({
+			dd"",
+
+		});
 	}
 }
 
