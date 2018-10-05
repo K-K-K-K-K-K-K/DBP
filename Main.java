82392928dd40eb9990cea79812a6e05393bc3210
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-10-05 09:39:20 +0900

    main

--- Main.java
+++ Main.java
@@ -6,39 +6,57 @@ public class Main {
 		System.out.println("DBP ~ DataBaseProgram~");
 		System.out.println();
 
-		// データベースの新規作成 | データベースファイル読み込み
-		Database db;
-		System.out.println("データベース: 新規作成(new) | 開く(open) | 一覧表示(list)");
-		System.out.print("> ");
-		Scanner sc = new Scanner(System.in);
-		switch (sc.next()) {
-			case "new":
-				System.out.println("データベース名 >");
-				db = new Database();
-				db.setName(sc.next());
-				break;
-			case "open":
-				System.out.println("データベース名 >");
-				db = new Database();
-				db.
-				break;
-			case "list":
-				break;
-			default:
-				System.out.println("エラー");
-		}
+		runShell();
+	}
 
-		// 中身dorの操作
-		s
-			casew "asddd" wwitch {
+	private static void runShell() {
+		Scanner sc = new Scanner(System.in);
+		String cmd;
+
+		for(;;) {
+			System.out.print("> ");
+			cmd = sc.nextLine();
+			switch (cmd) {
+				case "help":
+					printHelp();
+					System.out.println("");
+					break;
+				case "newdb":
+					System.out.println("");
+					break;
+				case "seldb":
+					System.out.println("");
+					break;
+				case "shwdb":
+					System.out.println("");
+					break;
+				case "exit":
+					System.out.println("");
+					System.exit(0);
+				case "":
+					break;
+				default:
+					System.out.println("[エラー] 存在しないコマンド");
+					System.out.println("    コマンド: " + cmd);
+					System.out.println("");
+			}
 		}
 	}
 
-	static void add() {
-		db.tbl["成績"].add({
-			dd"",
+	private static void printHelp() {
+		System.out.println("プログラム: ヘルプ(help) | 終了(exit)");
+		System.out.println("データベース: 新規作成(newdb) | 開く(seldb) | 一覧表示(shwdb)");
+		System.out.println("テーブル: 新規作成(newtbl) | 開く(seldbl) | 一覧表示(shwtbl)");
+		System.out.println("レコード: 新規作成(newrec) | 開く(selrec) |一覧表示((shwrec)");
+	}
 
-		});
+	private static void newDatabase() {
+	}
+
+	private static void selDatabase() {
+	}
+
+	private static void showDatabase() {
 	}
 }
 
