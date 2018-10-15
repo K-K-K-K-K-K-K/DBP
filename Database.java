45166348530147cd1e88910a07b21a00198bf702
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-10-15 17:38:15 +0900

    a

--- Database.java
+++ Database.java
@@ -2,5 +2,13 @@ import java.util.*;
 
 public class Database {
 	private List<Table> tbls = new ArrayList<>();
+
+	public void addTable(Table tbl) {
+		tbls.add(tbl);
+	}
+
+	public List<Table> getTables() {
+		return tbls;
+	}
 }
 
