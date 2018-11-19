5279b05d9bcdeadaa6a8ab667fed31e4b0829849
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-11-19 13:59:29 +0900

    aaa

--- Tests/DatabaseExceptionTest.java
+++ Tests/DatabaseExceptionTest.java
@@ -1,13 +1,9 @@
 public class DatabaseExceptionTest {
 	public static void main(String[] args) {
-		String msg = "試験文章";
 		try {
-			throw new DatabaseException(msg);
+			throw new DatabaseException("試験");
 		} catch (DatabaseException de) {
-			if (de.getMessage().equals(msg))
-				System.out.println("成功");
-			else
-				System.out.println("失敗");
+			System.out.println(de.getMessage());
 		}
 	}
 }
