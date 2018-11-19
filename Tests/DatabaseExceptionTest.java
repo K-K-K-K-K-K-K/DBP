0ee085822bcf14e352e6408ac929d9ea6d87650a
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-11-19 14:02:54 +0900

    bbb

--- Tests/DatabaseExceptionTest.java
+++ Tests/DatabaseExceptionTest.java
@@ -1,10 +1,12 @@
 public class DatabaseExceptionTest {
 	public static void main(String[] args) {
+		/* 例外発生試験 ----------------------------------------- */
 		try {
 			throw new DatabaseException("試験");
 		} catch (DatabaseException de) {
 			System.out.println(de.getMessage());
 		}
+		/* ------------------------------------------------------ */
 	}
 }
 
